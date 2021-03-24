2eaee28e3ec21da201845154fa4c3cd067c9be44
Jakub Haliniak <jakubh.fake@gmail.com>
2021-03-25 00:41:37 +0100

    Trying to put function displayResult

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'kamień';
+    }
+    else if(argMoveId == 2){
+        return 'papier';
+    }
+    else if(argMoveId == 3){
+        return 'nożyczki';
+    }
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+  }
 
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
 
-if(playerInput == '1'){
+let argPlayerMove = getMoveName(playerInput);
+console.log('dane do display ' + argPlayerMove);
+/*if(playerInput == '1'){
   playerMove = 'kamień';
 }
 else if (playerInput == '2'){
@@ -16,15 +30,16 @@ else if (playerInput == '3'){
 else {
     playerMove = 'nieznany ruch';
 }
-printMessage('Twój ruch to: ' + playerMove);
+*/
+printMessage('Twój ruch to: ' + argPlayerMove);
 
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let argComputerMove = getMoveName(randomNumber);
 
-if(randomNumber == 1){
+/*if(randomNumber == 1){
   computerMove = 'kamień';
 }
 else if (randomNumber == 2){
@@ -35,17 +50,59 @@ else if (randomNumber == 3){
 }
 else {
     computerMove = 'nieznany ruch';
-}
-printMessage('Mój ruch to: ' + computerMove);
+}*/
+printMessage('Ruch Bota to: ' + argComputerMove);
+printMessage('Bot zagrał ' + argComputerMove + ' a Ty ' + argPlayerMove);
 
-if( computerMove == 'kamień' && playerMove == 'papier'){
+printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+
+function displayResult(argPlayerMove){
+    
+      if(argPlayerMove == 2){
+      printMessage('Ty wygrywasz!');
+    
+    }
+}
+/*function displayResult(argComputerMove, argPlayerMove) {
+    console.log('displayResult');
+    printMessage('Bot zagrał ' + argComputerMove + ' a Ty ' + argPlayerMove);
+
+ /*if( argComputerMove == 'kamień' && argPlayerMove == 'papier' ){
+        printMessage('Ty wygrywasz!');
+      }
+      else if(argComputerMove == 'kamień' && argPlayerMove == 'nożyczki'){
+        printMessage('Wygrywa Bot!');
+      }
+      else if( argComputerMove == 'papier' && argPlayerMove == 'kamień'){
+        printMessage('Wygrywa Bot!');
+      }
+      else if( argComputerMove == 'papier' && argPlayerMove == 'nożyczki'){
+        printMessage('Ty wygrywasz!');
+      }
+      else if( argComputerMove == 'nożyczki' && argPlayerMove == 'kamień'){
+        printMessage('Ty wygrywasz!');
+      }
+      else if( argComputerMove == 'nożyczki' && argPlayerMove == 'papier'){
+        printMessage('Wygrywa Bot!');
+      }
+      elseif( argComputerMove == argPlayerMove ){
+        printMessage('Jest remis, graj dalej!!!');
+      }
+    else {
+        printMessage('Wynik gry to : Neznany ruch gracza, graj dalej!!!');
+        return 'nieznany ruch';
+      }
+}*/
+console.log('funkcja ' + displayResult);
+
+/*if( computerMove == 'kamień' && playerMove == 'papier'){
     printMessage('Ty wygrywasz!');
   }
 else if( computerMove == 'kamień' && playerMove == 'nożyczki'){
-    printMessage('Wygrywa komputer!');
+    printMessage('Wygrywa Bot!');
   }
 else if( computerMove == 'papier' && playerMove == 'kamień'){
-    printMessage('Wygrywa komputer!');
+    printMessage('Wygrywa Bot!');
   }
   else if( computerMove == 'papier' && playerMove == 'nożyczki'){
     printMessage('Ty wygrywasz!');
@@ -54,11 +111,11 @@ else if( computerMove == 'papier' && playerMove == 'kamień'){
     printMessage('Ty wygrywasz!');
   }
   else if( computerMove == 'nożyczki' && playerMove == 'papier'){
-    printMessage('Wygrywa komputer!');
+    printMessage('Wygrywa Bot!');
   }
 else if( computerMove == playerMove){
     printMessage('Jest remis, graj dalej!!!');
   }
-  else {
+else {
     printMessage('Wynik gry to : Neznany ruch gracza, graj dalej!!!');
-  }
+  }*/
