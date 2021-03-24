6ee5fdfa6bd8b319668aec96ddc932cc62780bf4
Jakub Haliniak <jakubh.fake@gmail.com>
2021-03-24 19:23:44 +0100

    JS if..else

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ let playerMove = 'papier';
 
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to' + playerMove + ', to wygrywasz!');
 
+let randomFraction = Math.random();
+
+printMessage('Wylosowany ułamek to: ' + randomFraction);
+
+let calculation = randomFraction * 3 + 1;
+
+printMessage('Ułamek pomnożony przez 3 i powiększony o 1: ' + calculation);
+
+let roundNumber = Math.floor(calculation);
+
+printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
